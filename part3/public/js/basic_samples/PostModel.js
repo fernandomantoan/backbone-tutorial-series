686f123633f730c99836763712161c1cd0b9c407
Fernando Mantoan <contato@fernandomantoan.com>
2012-08-02 17:55:15 -0300

    Updating samples

--- part3/public/js/basic_samples/PostModel.js
+++ part3/public/js/basic_samples/PostModel.js
@@ -77,3 +77,77 @@ post.destroy({
         window.alert('Ocorreu um erro');
     }
 });
+
+var post = new PostModel();
+post.on('change', function() {
+    console.log('O evento change foi disparado');
+});
+post.on('sync', function() {
+    console.log('O evento sync foi disparado');
+});
+post.set({
+    title: "Um titulo",
+    text: "Um texto "
+}); // Dispara change
+post.fetch(); // Dispara change e sync
+
+
+var post = new PostModel();
+post.on('change', function() {
+    if (post.hasChanged('title'))
+        console.log('o atributo titulo foi alterado');
+    else
+        console.log('o atributo titulo não foi alterado');
+});
+post.set({
+    title: 'Evento manual',
+    text: 'Texto'
+}, {
+    silent: true
+});
+post.change();
+
+var post = new PostModel({
+    title: 'Valor antigo',
+    text: 'Texto antigo'
+});
+
+post.on('change', function() {
+    if (post.hasChanged('title')) {
+        console.log('o atributo titulo foi alterado');
+        console.log(post.previous('title'));
+        console.log(post.get('title'));
+    }
+});
+post.set({title: 'Novo valor'});
+post.change();
+
+
+var PostModel = Backbone.Model.extend({
+    urlRoot: 'posts',
+    defaults: {
+        title: "",
+        text: ""
+    },
+    validate: function(attrs) {
+        if (attrs.title == '')
+            return 'O título é obrigatório';
+        if (attrs.text == '')
+            return 'O texto é obrigatório'
+    }
+});
+
+var post = new PostModel();
+post.on('error', function(model, error) {
+    alert(error);
+});
+post.save();
+
+var post = new PostModel();
+post.on('error', function(model, error) {
+    if (!model.isValid())
+        alert('Erro de validação: ' + error);
+    else
+        alert(error);
+});
+post.save();
